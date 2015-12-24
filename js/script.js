5585b5f7b8ab1bdfaae31d045dce99e5b896c6e6
Victorgichohi <victorgichohi48@gmail.com>
2015-12-24 12:04:28 +0300

    styled the nav-bar to become white after a certain scroll widht

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@
 //        $('.opaque-navbar').removeClass('opaque');
 //    }
 // });
+// $('.carousel').carousel({
+//   interval: 6000,
+//   pause: "false"
+// });
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $('#custom-nav').addClass('affix');
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $('#custom-nav').removeClass('affix');
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
